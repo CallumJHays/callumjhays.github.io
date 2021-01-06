3b1666e96a52729dfad29148087ab6be6ab2e58b
CallumJHays <callumjhays@gmail.com>
2021-01-07 01:32:38 +1000

    fix mdx regression

--- next.config.js
+++ next.config.js
@@ -1,13 +1,10 @@
 // next.config.js
 const withMdxEnhanced = require("next-mdx-enhanced");
-const withImageElement = require("next-image-element");
 
-module.exports = withImageElement(
-  withMdxEnhanced({
-    defaultLayout: true,
-    remarkPlugins: [],
-    rehypePlugins: [],
-  })({
-    pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  })
-);
+module.exports = withMdxEnhanced({
+  defaultLayout: true,
+  remarkPlugins: [],
+  rehypePlugins: [],
+})({
+  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
+});
