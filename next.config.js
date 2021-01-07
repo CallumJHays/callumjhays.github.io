3eb4978a51d484a794482adb87f29d9928f71389
CallumJHays <callumjhays@gmail.com>
2021-01-07 20:32:43 +1000

    add bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -3,11 +3,15 @@ const withMdxEnhanced = require("next-mdx-enhanced");
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 const withVideos = require("next-videos");
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 module.exports = withPlugins(
   [
     optimizedImages,
     withVideos,
+    withBundleAnalyzer,
     withMdxEnhanced({
       defaultLayout: true,
       remarkPlugins: [],
