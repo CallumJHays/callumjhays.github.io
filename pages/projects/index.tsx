2fe208c7229e86645bdf0a80414aabc6142ed07c
Callum J Hays <callumjhays@gmail.com>
2022-10-10 13:18:07 +1000

    fix: project index page responsive to small devices

--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -9,22 +9,23 @@ export default function ProjectIndexPage() {
   return (
     <MainLayout>
       <h1 className="my-10 ml-4 mb-20">Projects</h1>
-      <div className="grid grid-cols-5 gap-10">
+      <div className="flex flex-col items-center md:grid md:grid-cols-5 md:gap-10">
         {PROJECTS.map(
           ({ title, subtitle, imageURI, githubURL, tags, id }) => (
             <>
-              <div className="h-full col-span-2 mt-4">
+              <div className="md:col-span-2 mt-4 max-w-screen-sm" id={id}>
+                <h2 className="text-2xl mb-2 mt-8 md:hidden">{title}</h2>
                 <img src={imageURI} className="pb-2" />
-                <Marquee className="h-9" pauseOnHover={true} gradientWidth={20}>
+                <Marquee className="h-9 w-full" pauseOnHover={true} gradientWidth={20}>
                   {tags.map((tag) => (
                     <Pill>{tag}</Pill>
                   ))}
                 </Marquee>
               </div>
 
-              <div className="col-span-3 bg-white p-2">
-                <h2 className="text-2xl mb-2" id={id}>{title}</h2>
-                <p className="text-md mb-2">{subtitle}</p>
+              <div className="md:col-span-3 bg-white p-2 max-w-screen-sm">
+                <h2 className="text-2xl hidden md:block" id={id}>{title}</h2>
+                <p className="text-md mb-2 mt-2">{subtitle}</p>
                 {!githubURL ? null : (
                   <StyledLink href={githubURL}>Source Code (GitHub)</StyledLink>
                 )}
